0242bb47ccb901d30f17d13f5d21f8d0669a997e
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-06-14 12:53:05 +0000

    Code init

---
+++ server.js
@@ -0,0 +1,43 @@
+
+//Imports
+
+require("dotenv").config()
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require("path")
+const app = express()
+const mongoose = require("mongoose")
+// Router imports 
+
+const router_index = require("./routers/index")
+
+
+// DB
+
+mongoose.connect(process.env.MONGODB)
+.then(
+    console.log("✅| Database")
+)
+
+// Bodyparses
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// EJS 
+
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs")
+
+//Static
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// Routers
+
+
+app.use(router_index)
+
+//Export
+
+app.listen(3000)
