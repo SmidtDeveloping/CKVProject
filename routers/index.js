601e6187554a290ae2d56cafeb0bac9eeea3eed5
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-06-28 09:53:19 +0000

    Nieuwe dingetjessss

--- routers/index.js
+++ routers/index.js
@@ -5,4 +5,14 @@ Router.get("/", (req, res) => {
 res.render("index")
 })
 
+Router.get("/muziek", (req, res) => {
+    res.render("muziek")
+})
+Router.get("/dans", (req, res) => {
+    res.render("dans")
+})
+Router.get("/kunst", (req, res) => {
+    res.render("kunst")
+})
+
 module.exports = Router
