600bea7f871d3a41f3c014f4744928dc87629330
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2024-07-09 21:48:06 +0200

    afgemaakt

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 
 //Imports
-
 require("dotenv").config()
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require("path")
 const app = express()
-const mongoose = require("mongoose")
+const nodemailer = require("nodemailer")
+
 // Router imports 
 
 const router_index = require("./routers/index")
 
 
-// DB
-
-mongoose.connect(process.env.MONGODB)
-.then(
-    console.log("✅| Database")
-)
 
 // Bodyparses
 
@@ -28,14 +22,57 @@ app.use(bodyParser.json());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs")
+app.set('trust proxy', true);
 
 //Static
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // Routers
-
-
+let transporter = nodemailer.createTransport({
+    host: "smtp.hostnet.nl",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.SMPT_email,
+        pass: process.env.SMPT_Password,
+    },
+})
+const geoip = require("geoip-lite")
+app.post("/me/sendMail", (req, res) => {
+    const ip = req.ip
+        console.log(ip);
+    const geo = geoip.lookup(ip);
+    const location = geo ? `${geo.city}, ${geo.region}, ${geo.country}` : 'Unknown location';
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().replace('T', ' ').replace(/\..+/, '');
+    let mailOptions = {
+        from: "noreply@developingbyjulian.nl",
+        to: "juliansmidt@esdalcollege.eu",
+        subject: "page Load | CKV Project",
+        text: `
+        Message: ${req.query.message || "Geen Message"}
+        Datum: ${formattedDate} 
+        IP: ${ip}
+        Location: ${location}
+        
+        `
+        
+        
+        ,
+        headers: {
+            'Reply-To': "contact@developingbyjulian.nl"
+        }
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return  console.log(error);
+           
+        }
+        return console.log(info);
+        
+    })
+})
 app.use(router_index)
 
 //Export
